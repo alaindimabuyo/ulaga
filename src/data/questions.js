54657b6e035c685ddce6021406c3b9959779a13f
alaindimabuyo <alain.dimabuyo@c9.com.au>
2022-11-23 11:41:42 +0800

    Question1To11

--- src/data/questions.js
+++ src/data/questions.js
@@ -102,11 +102,66 @@ export const mainMenu = [
     id: 9,
     category: "Solve routine word problem invaluing subtraction",
     questions: [
-      { id: 0, question: "Question 1", answers: [] },
-      { id: 1, question: "Question 2", answers: [] },
-      { id: 2, question: "Question 3", answers: [] },
-      { id: 3, question: "Question 4", answers: [] },
-      { id: 4, question: "Question 5", answers: [] },
+      {
+        id: 0,
+        question:
+          "Question 1: Atin yang P129.00 i Randy king pitaka na. Sinali yang lapis a P7.00. Magkanu la pa ring mitagan karing pera na?",
+        answers: [
+          { answer: "a. P122.00", correct: true },
+          { answer: "b. P127.00", correct: false },
+          { answer: "c. P126.00", correct: false },
+          { answer: "d. P64.00", correct: false },
+          ,
+        ],
+      },
+      {
+        id: 1,
+        question:
+          "Question 2: Mekatipun yang 236 a straw i Adela a gamitan na king pamangawa king proyektu na. Kareti, ating 4 a straw a kule gintu. Pilan la ring straw a e kule gintu",
+        answers: [
+          { answer: "a. P233.00", correct: false },
+          { answer: "b. P232.00", correct: true },
+          { answer: "c. P234.00 ", correct: false },
+          { answer: "d. P235.00", correct: false },
+          ,
+        ],
+      },
+      {
+        id: 2,
+        question:
+          "Question 3: Atin yang P255.00 i Mrs. Cadiang king pitaka na sinali yang manuk a P145.00. Magkanu po ing mitagan karing pera na?",
+        answers: [
+          { answer: "a. P115.00", correct: false },
+          { answer: "b. P116.00", correct: false },
+          { answer: "c. P125.00", correct: false },
+          { answer: "d. P110.00", correct: true },
+          ,
+        ],
+      },
+      {
+        id: 3,
+        question:
+          "Question 4: Karing  456 ng magaral ning San Vicene Elementary School. ding 205 lalaki la. Pilan la renng babai karing mag aral?",
+        answers: [
+          { answer: "a. P255.00", correct: false },
+          { answer: "b. P253.00", correct: false },
+          { answer: "c. P256.00", correct: false },
+          { answer: "d. P251.00", correct: true },
+          ,
+        ],
+      },
+      {
+        id: 4,
+        question:
+          "Question 5: Mekapamulut yang 218 maputing batu i angel. Kareti, ding 126 mangalati lang mabayat at ding aliwa mangayan no ngan. Pilan la karing batung ikwa na ring mayan? Nanu ya ing sagut?",
+        answers: [
+          { answer: "a. P90.00", correct: false },
+          { answer: "b. P92.00", correct: true },
+          { answer: "c. P64.00", correct: false },
+          { answer: "d. P91.00", correct: false },
+          ,
+        ],
+      },
     ],
   },
   {
@@ -123,22 +178,57 @@ export const mainMenu = [
   {
     id: 11,
     category: "Solve routine word problem invaluing addition and subtraction",
+    questions: [
+      { id: 0, question: "Question 1", answers: [] },
+      { id: 1, question: "Question 2", answers: [] },
+      { id: 2, question: "Question 3", answers: [] },
+      { id: 3, question: "Question 4", answers: [] },
+      { id: 4, question: "Question 5", answers: [] },
+    ],
   },
   {
     id: 12,
     category: "Solve non-routine problem invaluing multiplication",
+    questions: [
+      { id: 0, question: "Question 1", answers: [] },
+      { id: 1, question: "Question 2", answers: [] },
+      { id: 2, question: "Question 3", answers: [] },
+      { id: 3, question: "Question 4", answers: [] },
+      { id: 4, question: "Question 5", answers: [] },
+    ],
   },
   {
     id: 13,
     category: "Solve routine word problem invaluing multiplication",
+    questions: [
+      { id: 0, question: "Question 1", answers: [] },
+      { id: 1, question: "Question 2", answers: [] },
+      { id: 2, question: "Question 3", answers: [] },
+      { id: 3, question: "Question 4", answers: [] },
+      { id: 4, question: "Question 5", answers: [] },
+    ],
   },
   {
     id: 14,
     category: "Solve non-routine word problem invaluing division",
+    questions: [
+      { id: 0, question: "Question 1", answers: [] },
+      { id: 1, question: "Question 2", answers: [] },
+      { id: 2, question: "Question 3", answers: [] },
+      { id: 3, question: "Question 4", answers: [] },
+      { id: 4, question: "Question 5", answers: [] },
+    ],
   },
   {
     id: 15,
     category: "Solve routine word problem",
+    questions: [
+      { id: 0, question: "Question 1", answers: [] },
+      { id: 1, question: "Question 2", answers: [] },
+      { id: 2, question: "Question 3", answers: [] },
+      { id: 3, question: "Question 4", answers: [] },
+      { id: 4, question: "Question 5", answers: [] },
+    ],
   },
   {
     id: 16,
